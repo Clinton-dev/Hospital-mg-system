9a73cf736961062756267a4e72c62882f8a39986
Clinton wambugu <clintonwambugu@gmail.com>
2022-11-11 22:47:30 +0300

    Add static files hospital app

--- hospital/views.py
+++ hospital/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import HospitalRegistration
 
-# Create your views here.
+
+def registerHospital(request):
+    if request.method == "POST":
+        form = HospitalRegistration(request.POST)
+        if form.is_valid():
+            form.save()
+            print('Hospital saved')
+            return redirect('/')
+    else:
+        form = HospitalRegistration()
+    return render(request, 'hospital/register.html', {"form": HospitalRegistration})
--- hospitalmg_system/urls.py
+++ hospitalmg_system/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hospital/', include('hospital.urls')),
     path('', include('dashboard.urls'))
 ]

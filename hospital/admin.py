695bc96d7156c2a8c38f320e8043a1b53bb6221d
Clinton wambugu <clintonwambugu@gmail.com>
2022-11-12 20:44:20 +0300

    Add subdepartment model

--- hospital/admin.py
+++ hospital/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Hospital)
+admin.site.register(models.Department)
--- hospital/models.py
+++ hospital/models.py
@@ -10,3 +10,20 @@ class Hospital(models.Model):
 
     def __str__(self):
         return f'{self.name} Hospital'
+
+
+class Department(models.Model):
+    name = models.CharField(null=True, max_length=150)
+    description = models.TextField(blank=False)
+    hospital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} Department'
+
+
+class SubDepartment(models.Model):
+    name = models.CharField(max_length=150)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.name} subdepartment'

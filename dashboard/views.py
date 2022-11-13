a9e67f6ccbd7360e1ff1dc4907a12608e7b10dc2
Clinton wambugu <clintonwambugu@gmail.com>
2022-11-13 23:08:43 +0300

    Add dynamic url

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from hospital.models import Department
 
 
 def home(request):
     return render(request, 'dashboard/home.html')
+
+
+def departments(request):
+    context = {
+        "departments": Department.objects.all()
+    }
+    return render(request, 'dashboard/departments.html', context)
